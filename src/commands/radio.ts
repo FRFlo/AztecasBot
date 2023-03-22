0a1a4be296f49f92d52f29f6843a06ef63cbbe62
FRFlo <62435410+FRFlo@users.noreply.github.com>
2023-03-22 13:53:11 +0100

    feat: Ajout radio automatiques

---
+++ src/commands/radio.ts
@@ -0,0 +1,43 @@
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle, ChatInputCommandInteraction, SlashCommandBuilder, TextChannel } from "discord.js";
+import { client, config, log } from "..";
+import { Embed } from "../classes/embed";
+import { AZCommand } from "../classes/files";
+
+module.exports = <AZCommand>{
+    data: new SlashCommandBuilder()
+        .setName("radio")
+        .setDescription("Régénérer la radio"),
+    async execute(interaction: ChatInputCommandInteraction) {
+        const date = Date.now();
+        log(`Radio changée à ${date.toLocaleString('fr-FR')}`)
+
+        for (const channelId in config.radios) {
+            const channel = client.channels.cache.get(config.radios[channelId]) as TextChannel;
+
+            if (!channel) {
+                log(`Impossible de trouver le channel ${config.radios[channelId]}`);
+                continue;
+            }
+
+            channel.send({
+                content: "@here",
+                embeds: [
+                    new Embed()
+                        .setTitle("Radio")
+                        .setDescription(`Holà todos, une nouvelle radio est disponible !\n\nPour rappel:\nIl est obligatoire de se brancher en radio en arrivant en ville. Ne pas être branché à celle çi peut vous attirer des sanctions`)
+                        .addFields([
+                            { name: "Radio actuelle", value: String(Math.floor(Math.random() * (100000 - 100 + 1) + 100)) },
+                        ])
+                ], components: [
+                    new ActionRowBuilder<ButtonBuilder>()
+                        .addComponents(
+                            new ButtonBuilder()
+                                .setCustomId("radio")
+                                .setLabel("Régénérer la radio")
+                                .setStyle(ButtonStyle.Danger)
+                        )
+                ]
+            })
+        }
+    },
+};
--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,6 +1,8 @@
-import { ActivityType, Client } from "discord.js";
-import { log } from "..";
+import { ActionRowBuilder, ActivityType, ButtonBuilder, ButtonStyle, Client, TextChannel } from "discord.js";
+import * as cron from "node-cron";
+import { config, log } from "..";
 import { AZEvent } from "../classes/files";
+import { Embed } from "../classes/embed";
 
 module.exports = <AZEvent>{
     name: 'ready',
@@ -17,5 +19,38 @@ module.exports = <AZEvent>{
                 }
             ]
         });
+
+        cron.schedule("0 17 * * *", (date: Date) => {
+            log(`Radio changée à ${date.toLocaleString('fr-FR', { timeZone: 'Europe/Paris' })}`)
+
+            for (const channelId in config.radios) {
+                const channel = client.channels.cache.get(config.radios[channelId]) as TextChannel;
+
+                if (!channel) {
+                    log(`Impossible de trouver le channel ${config.radios[channelId]}`);
+                    continue;
+                }
+
+                channel.send({
+                    content: "@here",
+                    embeds: [
+                        new Embed()
+                            .setTitle("Radio")
+                            .setDescription(`Holà todos, une nouvelle radio est disponible !\n\nPour rappel:\nIl est obligatoire de se brancher en radio en arrivant en ville. Ne pas être branché à celle çi peut vous attirer des sanctions`)
+                            .addFields([
+                                { name: "Radio actuelle", value: String(Math.floor(Math.random() * (100000 - 100 + 1) + 100)) },
+                            ])
+                    ], components: [
+                        new ActionRowBuilder<ButtonBuilder>()
+                            .addComponents(
+                                new ButtonBuilder()
+                                    .setCustomId("radio")
+                                    .setLabel("Régénérer la radio")
+                                    .setStyle(ButtonStyle.Danger)
+                            )
+                    ]
+                })
+            }
+        })
     },
 };
